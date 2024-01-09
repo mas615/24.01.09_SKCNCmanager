bd06321305d4ad6652fe3ae03dec818d91b81015
majun <mas9623@naver.com>
2024-01-09 16:08:50 +0900

    3th restart

---
+++ mysql/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+const mysql      = require('mysql2');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'skcncmanagerdb'
+});
+
+connection.connect();
+var dbtest
+connection.query('SELECT * from project_table', (error, rows, fields) => {
+  if (error) throw error;
+  console.log('User info is: ', rows);
+  dbtest = rows;
+});
+
+connection.end();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express majun', dbtest: dbtest  });
+});
+
+module.exports = router;
