c0eed93d7caa7096923270ab7c59648e021af384
majun <mas9623@naver.com>
2024-01-11 00:09:18 +0900

    24.01.10 last commit

--- skcncmanager/routes/m/m.js
+++ skcncmanager/routes/m/m.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const m = require('./m/m');
+const insert = require('./insert.js');
 
-router.use("/m",m);
+router.use("/insert", insert);
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
 
 module.exports = router;
---
+++ skcncmanager/무덤/index2.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var router = express.Router();
+
+const mysql      = require('mysql2');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'skcncmanagerdb'
+});
+var dbtest
+
+connection.connect();
+
+//connection.end();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  connection.query('SELECT * from project_table ORDER BY 1 DESC', (error, rows, fields) => {
+    if (error) throw error;
+    connection.query(`(SELECT project_code FROM project_table order by 1 desc LIMIT 1) union (select service_code from project_table order by 1 desc LIMIT 1) union (select manage_code from project_table where new_inspectiontype='A' order by 1 desc LIMIT 1) union (select manage_code from project_table where new_inspectiontype='B' order by 1 desc LIMIT 1) union (select manage_code from project_table where new_inspectiontype='C' order by 1 desc LIMIT 1) union (select manage_code from project_table where new_inspectiontype='D' order by 1 desc LIMIT 1) union (select manage_code from project_table where new_inspectiontype='E' order by 1 desc LIMIT 1) union (select manage_code from project_table where new_inspectiontype='F' order by 1 desc LIMIT 1)`, (error, serviceCodes, fields) => {
+      if (error) throw error;
+        var codefamily = []
+        for( const key of serviceCodes){
+          serviceCodes = key.project_code;
+          var currentNumber = parseInt(serviceCodes.substr(1), 10); // 'S006'에서 006을 추출하고 정수로 변환
+          var newNumber = currentNumber + 1;
+          var newCode = ('000' + newNumber).slice(-3); // 숫자를 다시 문자열로 변환하고 'S'를 추가
+          codefamily.push(newCode)
+        }
+        body = codefamily+"<input type='text'>";
+      res.render('index2', { title: 'SK C&C MANAGER2', head: 'head', body: body });
+    }); 
+  });    
+});
+
+router.post('/', (req, res, next) => {
+  var sql = "INSERT INTO project_table (project_code, service_code, manage_code, project_name, new_inspectiontype, old_inspectiontype, open_date, relative_comp, comp1, part1, manager1, manager1_phone, comp2, part2, manager2, manager2_phone, pentest, source_code, infra, note, check1, check2, check3, check4, check5, check6, check7, old_manage_code, old_project) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  var values = []
+  //받은 파라미터를 values에 추가
+  for (const key in req.body){
+    if (key === 17 || key === 18 || key === 19 || key === 21 || key === 22 || key === 23 || key === 24 || key === 25 || key === 26 || key === 27 || key === 29) {
+      values.push(Boolean(req.body[key]))
+    } else {
+      values.push(String(req.body[key]))
+    };
+  };
+  //쿼리 실행
+  connection.query(sql,values,function(err, rows, fields) {
+    if (err){
+      console.log(err);
+    }
+    else{
+      console.log(rows.insertId);
+    }
+  });
+  //302
+  res.redirect(302, "/");
+});
+
+
+module.exports = router;
