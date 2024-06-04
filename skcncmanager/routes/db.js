bab129bf189295b9d9de275f020fc78ce971e3d7
majun <mas9623@naver.com>
2024-06-04 09:37:16 +0900

    24.06.04 db createconnection > createpool

--- skcncmanager/routes/db.js
+++ skcncmanager/routes/db.js
@@ -1,8 +1,27 @@
 var mysql = require("mysql2");
-const db = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'root',
-    database : 'skcncmanagerdb'
-  }); 
-module.exports = db;
+// const db = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : 'root',
+//     database : 'skcncmanagerdb'
+//   }); 
+// module.exports = db;
+
+const pool = mysql.createPool({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'skcncmanagerdb',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+function query(sql, values, callback) {
+  pool.query(sql, values, (error, results, fields) => {
+    if (error) return callback(error);
+    callback(null, results, fields);
+  });
+}
+
+module.exports = { query };
